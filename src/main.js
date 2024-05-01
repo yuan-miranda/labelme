8225313f51cb5ac48e30b6e6482c9cdd3023c887
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-05-01 09:50:00 +0800

    initial commit.

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { createCanvas, loadImage } = require('canvas');
+
+const canvas = createCanvas(848, 477);
+const context = canvas.getContext('2d');
+
+const images = fs.readdirSync("./img");
+
+for (let i = 0; i < images.length; i++) {
+    loadImage(`./img/${images[i]}`).then((img) => {
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    
+        context.fillStyle = "white";
+        context.fillRect(790, 18, 50, 50);
+    
+        context.fillStyle = "black";
+        context.font = "bold 30px Arial";
+
+        if (i < 9) {
+            context.fillText(`0${i + 1}`, 800, 52);
+        }
+        else {
+            context.fillText(`${i + 1}`, 800, 52);
+        }
+        const buffer = canvas.toBuffer('image/png');
+        fs.writeFileSync(`./img/${images[i]}`, buffer);
+    });
+}
+
