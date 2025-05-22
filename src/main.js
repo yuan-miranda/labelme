faccee943352c861a7c0e816829e1dfd188ab680
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-05-23 07:48:03 +0800

    added some change

--- src/main.js
+++ src/main.js
@@ -1,29 +1,28 @@
 const fs = require('fs');
 const { createCanvas, loadImage } = require('canvas');
 
-// create img folder if it doesn't exist. (which is initially)
-if (!fs.existsSync("./img")) {
-    fs.mkdirSync("./img");
-} 
+if (!fs.existsSync("./img")) fs.mkdirSync("./img");
 
-const canvas = createCanvas(848, 477);
+const WIDTH = 848;
+const HEIGHT = 477;
+const canvas = createCanvas(WIDTH, HEIGHT);
+
 const context = canvas.getContext('2d');
-
 const images = fs.readdirSync("./img");
 
 for (let i = 0; i < images.length; i++) {
     loadImage(`./img/${images[i]}`).then((img) => {
         context.drawImage(img, 0, 0, canvas.width, canvas.height);
-    
+
         context.fillStyle = "white";
         context.fillRect(790, 18, 50, 50);
-    
+
         context.fillStyle = "black";
         context.font = "bold 30px Arial";
 
         if (i < 9) context.fillText(`0${i + 1}`, 800, 52);
         else context.fillText(`${i + 1}`, 800, 52);
-        
+
         const buffer = canvas.toBuffer('image/png');
         fs.writeFileSync(`./img/${images[i]}`, buffer);
     });
